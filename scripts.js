2255c4b0f082f8f4bd644de79253da4d13e647af
Andry Zirka <andryzirka@gmail.com>
2024-05-27 22:41:58 +0300

    new

--- scripts.js
+++ scripts.js
@@ -1,15 +1,34 @@
+// Сохраняем ссылки на неизменяемые элементы
 let prev = document.querySelector('.prev');
 let next = document.querySelector('.next');
+let sliderList = document.querySelector('.slider_list');
+let items = document.querySelectorAll('.slider_item');
+
+function moveNext() {
+    sliderList.appendChild(items[0]);
+    items = document.querySelectorAll('.slider_item');
+}
+
+function movePrev() {
+    sliderList.prepend(items[items.length - 1]);
+    items = document.querySelectorAll('.slider_item');
+}
 
 
-next.addEventListener("click", function () {
-    let items = document.querySelectorAll('.slider_item');
-    document.querySelector('.slider_list').appendChild(items[0]);
-
+document.addEventListener("click", function(event) {
+    let target = event.target.closest('button');
+    if (target === next) {
+        moveNext();
+    } else if (target === prev) {
+        movePrev();
+    }
 });
 
-prev.addEventListener("click", function () {
-    let items = document.querySelectorAll('.slider_item');
-    document.querySelector('.slider_list').prepend(items[items.length - 1]);
 
+window.addEventListener('wheel', function(event) {
+    if (event.deltaY > 0) {
+        moveNext();
+    } else {
+        movePrev();
+    }
 });
